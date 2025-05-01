025d09ddf7cf63579f2114589019e2f11fe174b5
Djibo Alhassane Ridwane <djiboridwanealhassane@gmail.com>
2025-05-01 23:12:50 +0100

    ajout du base_url

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 
-export const BASE_URL = "http://127.0.0.1:8000/api";
+export const BASE_URL = "https://backcheap.fivedev.store/public/api";
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
     withCredentials: true,
